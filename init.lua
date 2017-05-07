3dfda31c69565d287b86aed73808584cf0e9f30f
DS-Minetest <vorunbekannt75@web.de>
2017-05-07 12:30:51 +0200

    support all directions, not only down

--- init.lua
+++ init.lua
@@ -69,7 +69,7 @@ local function is_liquid(pos)
 	return false
 end
 
-local function get_connected(startpos, nodes, connected, VoxelManip, minp, maxp, hashed_pos)
+local function get_connected(startpos, nodes, connected, VoxelManip, minp, maxp, hashed_pos, dir)
 	local node = VoxelManip:get_node_at(startpos)
 	connected[hashed_pos] = nodes[node.name] or false
 	if not nodes[node.name] then
@@ -77,7 +77,7 @@ local function get_connected(startpos, nodes, connected, VoxelManip, minp, maxp,
 	end
 	local xyz = {"x", "y", "z"}
 	for p = 1, 3 do
-		for i = 1, -1, -2 do
+		for i = dir[2] * -1, dir[2], 2 * dir[2] do
 			local pos = vector.new(startpos)
 			pos[xyz[p]] = pos[xyz[p]] + i
 			hashed_pos = minetest.hash_node_position(pos)
@@ -86,7 +86,7 @@ local function get_connected(startpos, nodes, connected, VoxelManip, minp, maxp,
 				connected = get_connected(pos, nodes, connected,
 						VoxelManip, minp, maxp, hashed_pos)
 			end
-			if p == 2 then
+			if xyz[p] == dir[1] then
 				break
 			end
 		end
@@ -95,30 +95,31 @@ local function get_connected(startpos, nodes, connected, VoxelManip, minp, maxp,
 end
 
 -- Returns the pos of the liquid source that should be pumped next.
-local function get_liquid_pos(pos, liquid)
-	-- get all (to side or up) connected
+local function get_liquid_pos(pos, liquid, dir)
+	-- Get all (to side or to pumpdirection) connected.
 	local VoxelManip = minetest.get_voxel_manip()
 	local minp, maxp = VoxelManip:read_from_map(vector.add(pos, -50), vector.add(pos, 50))
 	local connected_o = get_connected(pos, {
 		[liquids[liquid].source] = "s",
 		[liquids[liquid].flowing] = "f",
-	}, {}, VoxelManip, minp, maxp, minetest.hash_node_position(pos))
+	}, {}, VoxelManip, minp, maxp, minetest.hash_node_position(pos), dir)
 	local connected = {}
 	for h, t in pairs(connected_o) do
 		if t == "s" then
 			connected[#connected+1] = minetest.get_position_from_hash(h)
 		end
 	end
-	-- choose the highest ones
+	-- Choose the nearest ones in altitude.
 	local outposss = {}
 	for i = 1, #connected do
-		if not outposss[1] or outposss[1].y < connected[i].y then
+		if not outposss[1] or (dir[2] < 0 and outposss[1][dir[1]] < connected[i][dir[1]])
+				or (dir[2] > 0 and outposss[1][dir[1]] > connected[i][dir[1]]) then
 			outposss = {connected[i]}
-		elseif outposss[1].y == connected[i].y then
+		elseif outposss[1][dir[1]] == connected[i][dir[1]] then
 			outposss[#outposss+1] = connected[i]
 		end
 	end
-	-- choose the ones that are most far away
+	-- Choose the ones that are most far away.
 	local outposs = {}
 	local maxdist = 0
 	for i = 1, #outposss do
@@ -128,21 +129,25 @@ local function get_liquid_pos(pos, liquid)
 			outposs[#outposs+1] = outposss[i]
 		end
 	end
-	-- if it are more than one, choose by random
+	-- If it are more than one, choose by random.
 	local outpos = outposs[math.random(#outposs)]
 	return outpos
 end
 
 local function step(pos)
 	local data = get_data(pos)
+	local xyz = {"x", "y", "z"}
+	data.dir = data.dir or -2
+	local dir = math.abs(data.dir)
+	dir = {xyz[dir], data.dir/dir}
 	if data.mode == "extend" then
 		local old_pos = vector.new(pos)
 		data.pipe_length = data.pipe_length or 0
-		pos.y = pos.y - data.pipe_length
+		pos[dir[1]] = pos[dir[1]] + (data.pipe_length * dir[2])
 		local node
 		repeat
 			data.pipe_length = data.pipe_length + 1
-			pos.y = pos.y - 1
+			pos[dir[1]] = pos[dir[1]] + dir[2]
 			node = minetest.get_node(pos)
 		until node.name ~= "pump:pipe"
 		set_data(old_pos, data)
@@ -176,17 +181,17 @@ local function step(pos)
 			return
 		end
 		local dstpos = vector.new(pos)
-		dstpos.y = dstpos.y + 1
+		dstpos[dir[1]] = dstpos[dir[1]] - dir[2]
 		if minetest.get_node(dstpos).name ~= "air" then
 			return
 		end
 		local srcpos = vector.new(pos)
 		data.pipe_length = data.pipe_length or 0
-		srcpos.y = srcpos.y - data.pipe_length
+		srcpos[dir[1]] = srcpos[dir[1]] + (data.pipe_length * dir[2])
 		local node = minetest.get_node(srcpos)
 		while node.name == "pump:pipe" do
 			data.pipe_length = data.pipe_length + 1
-			srcpos.y = srcpos.y - 1
+			srcpos[dir[1]] = srcpos[dir[1]] + dir[2]
 			node = minetest.get_node(srcpos)
 		end
 		set_data(pos, data)
@@ -197,14 +202,14 @@ local function step(pos)
 			step(pos)
 			return
 		end
-		srcpos = get_liquid_pos(srcpos, data.liquid)
+		srcpos = get_liquid_pos(srcpos, data.liquid, dir)
 		minetest.remove_node(srcpos)
 		minetest.set_node(dstpos, {name = liquids[data.liquid].source})
 	end
 end
 
-local function remove_pipes(pos)
-	pos.y = pos.y - 1
+local function remove_pipes(pos, dir)
+	pos[dir[1]] = pos[dir[1]] + dir[2]
 	minetest.after(0.4, function(pos)
 		if minetest.get_node(pos).name == "pump:pipe" then
 			minetest.remove_node(pos)
@@ -218,7 +223,7 @@ minetest.register_node("pump:pump", {
 	groups = {snappy=2, choppy=2, oddly_breakable_by_hand=2},
 	sounds = default.node_sound_wood_defaults(),
 	on_construct = function(pos)
-		set_data(pos, {mode = "extend", dir = "-y", pipe_length = 0}, true)
+		set_data(pos, {mode = "extend", dir = -2, pipe_length = 0}, true)
 	end,
 	--~ on_receive_fields = function(pos, formanme, fields, sender) -- TODO
 	--~ end,
@@ -226,7 +231,12 @@ minetest.register_node("pump:pump", {
 		step(pos)
 	end,
 	on_destruct = function(pos)
-		remove_pipes(pos)
+		local data = get_data(pos)
+		local xyz = {"x", "y", "z"}
+		data.dir = data.dir or -2
+		local dir = math.abs(data.dir)
+		dir = {xyz[dir], data.dir/dir}
+		remove_pipes(pos, dir)
 		set_data(pos, {}, true)
 	end,
 })
@@ -239,21 +249,62 @@ minetest.register_node("pump:pipe", {
 	sounds = default.node_sound_wood_defaults(),
 	drop = "",
 	on_destruct = function(pos)
-		remove_pipes(vector.new(pos))
-		local i = -1
-		local node
-		repeat
-			i = i + 1
-			pos.y = pos.y + 1
-			node = minetest.get_node(pos)
-		until node.name ~= "pump:pipe"
-		if node.name ~= "pump:pump" then
+		local dir = {"y", -1} -- Just some default values.
+		local pipe_near = minetest.find_node_near(pos, 1, "pump:pipe")
+		if not pipe_near then -- Set pipe_length to 0 if it's only one pipe.
+			local pump_near = minetest.find_node_near(pos, 1, "pump:pump")
+			if not pump_near then
+				return
+			end
+			local data = get_data(pump_near)
+			data.pipe_length = 0
+			data.mode = "extend"
+			set_data(pos, data, true)
 			return
 		end
-		local data = get_data(pos)
+		local posdif = vector.subtract(pos, pipe_near)
+		for p, v in pairs(posdif) do
+			if v ~= 0 then
+				dir[1] = p
+				dir[2] = -1 * v
+				break
+			end
+		end
+		-- Remove pipes away from pump.
+		local pu = vector.new(pos)
+		local pumppos
+		repeat
+			pu[dir[1]] = pu[dir[1]] + dir[2]
+			local node = minetest.get_node(pu)
+			if node.name ~= "pump:pipe" then
+				if node.name == "pump:pump" then
+					pumppos = pu
+					dir[2] = -1 * dir[2]
+				end
+				break
+			end
+		until false
+		remove_pipes(vector.new(pos), dir)
+		local i
+		if not pumppos then
+			pumppos = vector.new(pos)
+			i = -1
+			local node
+			repeat
+				i = i + 1
+				pumppos[dir[1]] = pumppos[dir[1]] - dir[2]
+				node = minetest.get_node(pumppos)
+			until node.name ~= "pump:pipe"
+			if node.name ~= "pump:pump" then
+				return
+			end
+		else
+			i = vector.distance(pos,pumppos) - 1
+		end
+		local data = get_data(pumppos)
 		data.pipe_length = i
 		data.mode = "extend"
-		set_data(pos, data)
+		set_data(pumppos, data, true)
 	end,
 })
 
